d397b2eedaecad58dc619d67871f2b7ba5a8f2e6
surey <komeijiddz@gmail.com>
2024-11-10 00:57:56 +0800

    Experiment results from smartbugs_20241109_235759

---
+++ .venv/bin/achecker.py
@@ -0,0 +1,7 @@
+#!/home/shuo/AChecker/.venv/bin/python3
+# EASY-INSTALL-DEV-SCRIPT: 'AChecker==0.1.0','achecker.py'
+__requires__ = 'AChecker==0.1.0'
+__import__('pkg_resources').require('AChecker==0.1.0')
+__file__ = '/home/shuo/AChecker/bin/achecker.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
